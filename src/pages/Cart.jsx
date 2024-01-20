c84ac998d549657179e98ea4e8d88f8553c4bf4d
Parth Madhvani <madhvaniparth2@gmail.com>
2024-01-21 00:25:28 +0530

    update UI

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -16,47 +16,47 @@ const Cart = () => {
 
   return (
     <div>
-  {
-    cart.length > 0  ? 
-    (<div>
-      <div>
-        {
-          cart.map( (item,index) => {
-            return <CartItem key={item.id} item={item} itemIndex={index} />
-          } )
-        }
-      </div>
+      {
+        cart.length > 0  ? 
+        (<div>
+          <div>
+            {
+              cart.map( (item,index) => {
+                return <CartItem key={item.id} item={item} itemIndex={index} />
+              } )
+            }
+          </div>
 
-      <div>
+          <div>
 
-        <div>
-          <div>Your Cart</div>
-          <div>Summary</div>
-          <p>
-            <span>Total Items: {cart.length}</span>
-          </p>
-        </div>
+            <div>
+              <div>Your Cart</div>
+              <div>Summary</div>
+              <p>
+                <span>Total Items: {cart.length}</span>
+              </p>
+            </div>
 
-        <div>
-          <p>Total Amount: ${totalAmount}</p>
-          <button>
-            CheckOut Now
-          </button>
-        </div>
+            <div>
+              <p>Total Amount: ${totalAmount}</p>
+              <button>
+                CheckOut Now
+              </button>
+            </div>
 
-      </div>
+          </div>
 
 
-    </div>) : 
-    (<div>
-      <h1>Cart Empty</h1>
-      <Link to={"/"}>
-        <button>
-          Shop Now
-        </button>
-      </Link>
-    </div>)
-  }
+        </div>) : 
+        (<div>
+          <h1>Cart Empty</h1>
+          <Link to={"/"}>
+            <button>
+              Shop Now
+            </button>
+          </Link>
+        </div>)
+      }
     </div>
   );
 };
